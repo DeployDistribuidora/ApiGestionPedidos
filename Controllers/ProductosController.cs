1b8523df261d05fba8fa6a99c30da303bd29f2b0
Gabriel-Vidal-Chevalier <gabriel.vidal.chevalier@gmail.com>
2024-12-28 17:50:23 -0300

    Vista Productos desde backend

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -1,49 +1,86 @@
 ﻿using Front_End_Gestion_Pedidos.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
 
 namespace Front_End_Gestion_Pedidos.Controllers
 {
     public class ProductosController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductosController(IHttpContextAccessor httpContextAccessor, IHttpClientFactory httpClientFactory)
         {
+            _httpContextAccessor = httpContextAccessor;
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            IEnumerable<Producto> productos = new List<Producto>();
+            // Recuperar usuario desde la sesión
+            var usuarioLogueado = _httpContextAccessor.HttpContext.Session.GetString("UsuarioLogueado");
+            var rolUsuario = _httpContextAccessor.HttpContext.Session.GetString("Role");
+
+            if (string.IsNullOrEmpty(usuarioLogueado))
+                return RedirectToAction("Login", "Account");
+
             // Obtener todos los productos
-            //var productos = ObtenerProductos();
+            productos = await ObtenerProductos();
 
             // Retornar la vista con todos los productos
-            return View();
+            return View(productos);
         }
 
-        //[HttpGet]
-        //public IActionResult BuscarProductos(string searchTerm)
-        //{
-        //    // Obtener todos los productos
-        //    //var productos = ObtenerProductos();
+        [HttpGet]
+        public async Task<IActionResult> BuscarProductos(string searchTerm)
+        {
+            // Obtener todos los productos
+            var productos = await ObtenerProductos();
 
-        //    // Filtrar los productos según el término de búsqueda
-        //    if (!string.IsNullOrEmpty(searchTerm))
-        //    {
-        //        productos = productos.Where(p =>
-        //            p.Nombre.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)).ToList();
-        //    }
+            // Filtrar los productos según el término de búsqueda
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                productos = productos.Where(p =>
+                    p.Descripcion.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
 
-        //    ViewBag.SearchTerm = searchTerm; // Mantener el término de búsqueda en la vista
+            ViewBag.SearchTerm = searchTerm; // Mantener el término de búsqueda en la vista
 
-        //    // Retornar la vista con los productos filtrados
-        //    return View("Index", productos);
-        //}
+            // Retornar la vista con los productos filtrados
+            return View("Index", productos);
+        }
 
-        //private List<Producto> ObtenerProductos()
-        //{
-        //    // Simulación de datos
-        //    return new List<Producto>
-        //    {
-        //        new Producto { Id = 1, Nombre = "Papel higénico", Stock = 100, Precio = 25.50m },
-        //        new Producto { Id = 2, Nombre = "Jabón", Stock = 50, Precio = 15.00m },
-        //        new Producto { Id = 3, Nombre = "Pata dental", Stock = 0, Precio = 30.75m },
-        //        new Producto { Id = 4, Nombre = "Esponjas", Stock = 200, Precio = 10.00m },
-        //        new Producto { Id = 5, Nombre = "Escobas", Stock = 300, Precio = 50.00m }
-        //    };
-        //}
+        private async Task<IEnumerable<Producto>> ObtenerProductos()
+        {
+            //var model = new PedidoViewModel();
+            IEnumerable<Producto> productosResponse = new List<Producto>();
+
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7078/api/Stocks");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                productosResponse = JsonSerializer.Deserialize<List<Producto>>(jsonResponse, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                //model.Pedidos = pedidosResponse ?? new List<Pedido>();
+                //pedidos = model.Pedidos;
+            }
+            else
+            {
+                // Manejar errores en la solicitud a la API
+                ModelState.AddModelError(string.Empty, "Error al obtener los productos.");
+                return null;
+            }
+
+            return productosResponse;
+        }
+
+
     }
 }
--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,10 +1,31 @@
-﻿namespace Front_End_Gestion_Pedidos.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Front_End_Gestion_Pedidos.Models
 {
     public class Producto
     {
         public string Codigo { get; set; }
         public string Descripcion { get; set; }
+        public string Marca { get; set; }
         public double Stock_Actual { get; set; }
+        public double Stock_Minimo { get; set; }
+        public string Estante { get; set; }
+        public string Procedencia { get; set; }
+        public byte Clase { get; set; }
+        public byte Moneda { get; set; }
+        public byte MonLista { get; set; }
+        public byte Tasa { get; set; }
+        public double Stock_2 { get; set; }
+        public double Stock_3 { get; set; }
+        public double Stock_4 { get; set; }
+        public double Stock_5 { get; set; }
         public double? Precio { get; set; }
+        public double? Costo { get; set; }
+        public string? Detalle { get; set; }
+        public string? CodBarra { get; set; }
+        public double? Porcentaje { get; set; }
+        public long? Similar { get; set; }
+        public DateTime? F_Compra { get; set; }
+        public byte? Estado { get; set; }
     }
 }
